3a6e7461f3dc828a8a569ca0c8691627293d9f5d
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-04-30 13:39:34 -0400

    Add closing semicolons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var $showHide = $('.btn-show-hide');
+var $inOut = $('.btn-move');
 
 $showHide.on('click', function () {
-  var $box = $('.box')
+  var $box = $('.box');
 
-  $box.toggleClass('box-show')
-})
+  $box.toggleClass('box-show');
+});
+
+$inOut.on('click', function () {
+  var $diamond = $('.diamond');
+
+});
